40db7f1ce1160dd5e5c05269e3cfc030e9bc507c
cdshenlong1 <cdshenlong1@jd.com>
2021-04-15 10:37:41 +0800

    [iniy]

---
+++ src/main/java/com/athena/springone2020/Springone2020Application.java
@@ -0,0 +1,13 @@
+package com.athena.springone2020;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springone2020Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springone2020Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/athena/springone2020/Springone2020ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.athena.springone2020;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springone2020ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
